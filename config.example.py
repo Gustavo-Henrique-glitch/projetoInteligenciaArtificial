a123b27d6247a7cda61b6d2f5539583ebaf94e8a
Gustavo Henrique <gh33493@gmail.com>
2026-08-30 16:53:07 -0300

    criação de modelo de arquivo de config.py

---
+++ config.example.py
@@ -0,0 +1,7 @@
+# config.example.py
+# Copie este arquivo, renomeie para "config.py", e ajuste os caminhos
+# para onde você salvou as bases de dados na SUA máquina.
+
+RAIZ_COVID = "/caminho/para/seu/dataset/COVID-19_Radiography_Dataset/"
+RAIZ_PNEUMONIA = "/caminho/para/seu/dataset/chest_xray/"
+RAIZ_TUBERCULOSE = "/caminho/para/seu/dataset/TB_Chest_Radiography_Database/"
